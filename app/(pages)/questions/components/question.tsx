3b9a123ab28390210cd43966b2350c144f43f80d
diegonicita <diegonicita@gmail.com>
2024-01-07 19:22:30 -0300

    Add searchbar component to questions page

--- app/(pages)/questions/components/question.tsx
+++ app/(pages)/questions/components/question.tsx
@@ -1,12 +1,41 @@
 'use client'
 
-import { useGetQuestionByIdQuery } from '@/app/lib/services/choice'
+// import { useGetQuestionByIdQuery } from '@/app/lib/services/choice'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import Searchbar from './searchbar'
 
 export default function QuestionPage() {
-  // Using a query hook automatically fetches data and returns query values
-  const { data, error, isLoading } = useGetQuestionByIdQuery(5)
+  
+  // const { data, error, isLoading } = useGetQuestionByIdQuery(5)
+  const isLoading = false
+  const data = {
+    id: 1,
+    texto: '¿Cuantos huesos, aproximadamente, tiene un ser humano?',
+    opcion1: '100 huesos',
+    opcion2: '200 huesos',
+    opcion3: '300 huesos',
+    opcion4: '400 huesos',   
+  }
+
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+  const id = searchParams.get('search') as string
+  console.log('search', id)
+
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams)
+    if (term) {
+      params.set('search', term)
+    } else {
+      params.delete('search')
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }
+
   return (
     <>
+      <Searchbar handleSearch={handleSearch} />
       <h1 className="font-bold mb-4">
         Pregunta Id: <span>{!isLoading && data?.id}</span>
       </h1>
