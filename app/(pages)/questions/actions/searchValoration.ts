594b7bd7c1fddc28443a5a999eb47a57970f079f
diegonicita <diegonicita@gmail.com>
2024-01-17 17:26:04 -0300

    Fix type bug in questions page

--- app/(pages)/questions/actions/searchValoration.ts
+++ app/(pages)/questions/actions/searchValoration.ts
@@ -3,7 +3,7 @@ import { RowDataPacket } from 'mysql2'
 import searchOwnValoration from './searchOwnValoration'
 import searchValorationsQuery from './Queries/searchValorationsQuery'
 
-const searchValorations = async (results: RowDataPacket | undefined) => {
+const searchValorations = async (results: RowDataPacket | undefined | null) => {
   const ids = results?.map((r: { id: any }) => r.id)
   try {
     const result = await searchValorationsQuery(ids)
