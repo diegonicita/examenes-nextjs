135564ce242ebf15d7556460bc288487e009d64c
diegonicita <diegonicita@gmail.com>
2024-01-23 15:37:08 -0300

    Add get-profile api route

---
+++ app/api/get-profile/route.ts
@@ -0,0 +1,34 @@
+import getInfoAuthCookie from '@/app/server-actions/helpers/getInfoAuthCookie'
+export const dynamic = 'force-dynamic' // defaults to auto
+
+export async function GET(req: Request, res: Response) {
+  const { searchParams } = new URL(req.url)
+  let id = searchParams.get('id')
+  const payload = (await getInfoAuthCookie()) as any
+  const token = payload?.token
+  const requestOptions = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+
+  if (payload?.role === 'admin' && process.env.URL_PROFILE) {
+    if (!id) id = payload.id
+    const respuesta = await fetch(
+      process.env.URL_PROFILE + '/' + id,
+      requestOptions,
+    )
+    const json = await respuesta.json()
+    return Response.json(json)
+  }
+
+  if (payload?.role === 'client' && process.env.URL_PROFILE && payload?.id) {
+    const respuesta = await fetch(
+      process.env.URL_PROFILE + '/' + payload.id,
+      requestOptions,
+    )
+    const json = await respuesta.json()
+    return Response.json(json)
+  }
+  return Response.json({ error: 'No estas autorizado' })
+}
