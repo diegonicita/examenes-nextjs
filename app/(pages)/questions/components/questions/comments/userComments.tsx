5c96d7f9522223df047af090b053a4dc72b48e13
Ever Jose Rojas perez <everjosejr@hotmail.com>
2024-01-23 21:30:01 -0500

    add-styles in usercomments

--- app/(pages)/questions/components/questions/comments/userComments.tsx
+++ app/(pages)/questions/components/questions/comments/userComments.tsx
@@ -1,16 +1,31 @@
 export default function UserComments() {
   return (
-    <section className="flex flex-col">
-      <div className="flex flex-row justify-center items-center">
-        <div className="flex-grow">
-          <h1 className="text-sm ">Ever Rojas</h1>
-        </div>
-        <h2 className="text-sm">5min</h2>
-        <div className="flex justify-center items-center">
-        <h3 className="text-sm ">...</h3>
-        </div>
+    <section className="grid grid-cols-[40px,1fr] mt-5 ">
+      <div className="">
+        <img
+          alt="profile"
+          src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+          className="w-10 h-10 rounded-full"
+        />
       </div>
-      <p>this is the most famous comment in examens</p>
+      <main className="flex flex-col">
+        <div className=" card w-full p-3  bg-gray-200 shadow-xl ml-2">
+          <div className="flex flex-row">
+            <div className="flex-grow">
+              <h1 className="text-sm ">Ever Rojas</h1>
+            </div>
+            <h2 className="text-sm">5min</h2>
+            <h3 className="text-sm ">...</h3>
+          </div>
+          <div className="">
+            <p>this is the most famous comment in examens</p>
+          </div>
+        </div>
+        <div className="flex flex-row gap-2 ml-2 mt-2">
+          <span>que opinas</span>
+          <span>respuesta</span>
+        </div>
+      </main>
     </section>
   );
 }
