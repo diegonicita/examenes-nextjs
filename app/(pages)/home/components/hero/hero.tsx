e70b93856064b695b671ffc1f57fceb4c4509a31
diegonicita <diegonicita@gmail.com>
2024-01-26 16:38:34 -0300

    Fix bugs in home. Refactor home

--- app/(pages)/home/components/hero/hero.tsx
+++ app/(pages)/home/components/hero/hero.tsx
@@ -29,9 +29,9 @@ export default function Hero() {
             className="text-wrap-balance max-w-[20rem] text-center text-gray-500 
         text-sm font-normal font-inter leading-relaxed mt-5 mb-[25px] md:text-left"
           >
-            Practica con nuestros examenes de residencias medicas. Mas de 5000
-            preguntas disponibles de examenes de Argentina, España, México y
-            Perú
+            Practica con nuestros exámenes de residencias médicas. Más de 5000
+            preguntas disponibles de exámenes de Argentina, España, México y
+            Perú.
           </p>
           <ButtonLogin />
         </section>
--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -14,7 +14,7 @@ const Home = () => {
           <ConsultForm />
         </div>
       </div>
-      <Review />
+      {/* <Review /> */}
     </>
   )
 }
