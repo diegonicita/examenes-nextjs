558209ba7590bd3f5ea9c6a00579644297e6f35b
diegonicita <diegonicita@gmail.com>
2024-01-31 16:26:01 -0300

    Fix getExamBySubjectQuery parameters

--- app/(pages)/subjects/actions/queries/getExamBySubjectQuery.ts
+++ app/(pages)/subjects/actions/queries/getExamBySubjectQuery.ts
@@ -7,16 +7,17 @@ export default async function getExamBySubjectQuery(
   subjectId: number,
   limit: number,
 ) {
+  console.log(subjectId)
   const queryString1 =
     'SELECT * FROM preguntas WHERE examen = ? AND (clasifica1 = ? OR clasifica2 = ? OR clasifica3 = ? OR clasifica4 = ? OR clasifica5 = ?) LIMIT ?'
   const result = (await executeQuery(queryString1, [
-    examId,
-    subjectId,
-    subjectId,
-    subjectId,
-    subjectId,
-    subjectId,
-    limit,
+    examId.toString(),
+    subjectId.toString(),
+    subjectId.toString(),
+    subjectId.toString(),
+    subjectId.toString(),
+    subjectId.toString(),
+    limit.toString(),
   ])) as RowDataPacket
   return result
 }
