aa5e89b6003ee308ebd683264ddbb532b1b38fd6
diegonicita <diegonicita@gmail.com>
2024-02-15 17:34:56 -0300

    Add unauthorized control to deleteComments

--- app/(pages)/questions/actions/deleteComments/deleteComments.ts
+++ app/(pages)/questions/actions/deleteComments/deleteComments.ts
@@ -1,35 +1,52 @@
-"use server";
-import executeQuery from "@/app/server-actions/helpers/mysqldb";
-
-import { revalidatePath } from "next/cache";
-import { z } from "zod";
+'use server'
+import executeQuery from '@/app/server-actions/helpers/mysqldb'
+import getInfoAuthCookie from '@/app/server-actions/helpers/getInfoAuthCookie'
+import { RowDataPacket } from 'mysql2'
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
+import { UserType } from '@/app/models/User'
 
 const schema = z.object({
   id: z.string(),
-});
+})
 export default async function DeleteComment(
   prevState: { message: string },
-  formData: FormData
+  formData: FormData,
 ) {
   const validateFields = schema.safeParse({
-    id: formData.get("id"),
-  });
-  console.log(validateFields);
+    id: formData.get('id'),
+  })
+  console.log(validateFields)
   if (!validateFields.success) {
     return {
       errors: validateFields.error.flatten().fieldErrors,
-    };
+    }
   }
   try {
+    // Obtener información del comentario
+    const commentInfo = (await executeQuery(
+      'SELECT id_user FROM comments WHERE id = ?',
+      [validateFields.data.id],
+    )) as RowDataPacket[]
+    const commentUserId = commentInfo[0].id_user
+    // Obtener informacion del usuario
+    const currentUser = (await getInfoAuthCookie()) as UserType
+    const role = currentUser.role
+    const userId = currentUser.id
+    // Comparar su role
+    // Comparar id de usuario con el id del comentario
+    if (role === 'user' && commentUserId !== userId) {
+      console.log('Unauthorized')
+      return { message: 'unauthorized' }
+    }
     // Delete comments from the main table based on the condition
-    (await executeQuery(
-      "DELETE FROM comments WHERE id = ?",
-      [validateFields.data.id]
-    )) 
-    revalidatePath("/");
-    console.log("success");
-    return { message: "success" };
+    await executeQuery('DELETE FROM comments WHERE id = ?', [
+      validateFields.data.id,
+    ])
+    revalidatePath('/')
+    console.log('success')
+    return { message: 'success' }
   } catch (e) {
-    return { message: "error" };
+    return { message: 'error' }
   }
 }
