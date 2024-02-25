34153149814680d4eeee4c2fc51f3853542caacb
Ever Jose Rojas perez <everjosejr@hotmail.com>
2024-02-25 16:56:50 -0500

    fix-types in typescript

--- app/(pages)/questions/actions/reportComment/reportComent.ts
+++ app/(pages)/questions/actions/reportComment/reportComent.ts
@@ -1,5 +1,6 @@
 "use server";
 import executeQuery from "@/app/server-actions/helpers/mysqldb";
+import { RowDataPacket } from "mysql2";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 const schema = z.object({
@@ -27,16 +28,16 @@ export default async function reportComment(
     const commentData = await executeQuery(
       "SELECT id, id_user FROM comments WHERE id = ?",
       [validateFields.data.id]
-    );
+    )as RowDataPacket
     console.log(commentData[0].id)
 
     // Step 2: Insert data into reportcoments
     if (commentData && commentData.length > 0) {
         // Step 2: Insert data into reportcomments
         await executeQuery(
-          "INSERT INTO reportcomments (id_user, id, reporting_comments) VALUES (?, ?, ?)",
-          [commentData[0].id_user, commentData[0].id, validateFields.data.report]
-        );
+          "INSERT INTO reportcomments (id_user, id, reporting_comments) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE reporting_comments = ?",
+          [commentData[0]?.id_user, commentData[0].id, validateFields.data.report,validateFields.data.report]
+        ) 
         revalidatePath("/")
     } else {
         return { message: "No data found for the specified comment ID" };
