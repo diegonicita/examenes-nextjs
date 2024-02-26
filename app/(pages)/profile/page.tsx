8134ce6b9732ffeaa8d0c0977be2a7603297dbdc
diegonicita <diegonicita@gmail.com>
2024-02-26 15:23:36 -0300

    config next-auth and github

--- app/(pages)/profile/page.tsx
+++ app/(pages)/profile/page.tsx
@@ -4,25 +4,35 @@ import getInfoAuthCookie from '@/app/server-actions/helpers/getInfoAuthCookie'
 import Counter from './components/counter'
 import Message from './components/message'
 import MessageNotLogged from '@/app/components/checkCookie/messageNotLogged'
+import { auth } from '@/app/auth'
 
 const Profile = async () => {
   try {
     const authData = (await getInfoAuthCookie()) as UserType
-    
+    const session = await auth()
     if (authData) {
       return (
-        <div className="flex flex-col items-center mt-4 justify-center">
-          <div className="bg-base-200 p-2 rounded">
-            <img
-              src={
-                'https://mercado.webapp.ar/images_avatar/avatar-001-normal.png'
-              }
-            />
-            <div className="text-xl pb-4 font-bold text-center">Perfil del Usuario</div>
+        <div className="flex mt-4 gap-4 text-center justify-center">
+          <div className="bg-base-200 p-2 gap-4">
+            {authData && (
+              <img
+                src={
+                  'https://mercado.webapp.ar/images_avatar/avatar-001-normal.png'
+                }
+              />
+            )}
+            <div className="font-bold">CREDENTIALS</div>
             <div>ID: {authData && authData.id?.toString()}</div>
             <div>Nombre: {authData && authData.username}</div>
             <div>Email: {authData && authData.email}</div>
             <div>Rol: {authData && authData.role} </div>
+          </div>
+          <div className="bg-base-200 p-2">
+            {session && session.user && (
+              <img className="rounded-full" src={session.user?.image} />
+            )}
+            <div className="font-bold"> GITHUB: </div>
+            <div>Nombre: {session?.user?.name}</div>
           </div>
           {/* <Counter />
           <Message /> */}
