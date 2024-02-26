f0be638e0199e9f2764f2ff449c9c7fa2812abc2
diegonicita <diegonicita@gmail.com>
2024-02-26 00:53:07 -0300

    add loader in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Nav } from './components/navbar/navbar'
 import { Footer } from './components/footer/footer'
 import './styles/globals.css'
 import { Toaster } from 'react-hot-toast'
-import { ClerkProvider, auth } from '@clerk/nextjs'
+import { ClerkLoaded, ClerkLoading, ClerkProvider, auth } from '@clerk/nextjs'
 import getInfoAuthCookie from './server-actions/helpers/getInfoAuthCookie'
 
 export default async function RootLayout(props: React.PropsWithChildren) {
@@ -16,11 +16,34 @@ export default async function RootLayout(props: React.PropsWithChildren) {
       <ClerkProvider>
         <html lang="en">
           <body className="block">
-            <Nav logged={payload ? true : false} />
-            <Toaster />
-            <section className="min-h-screen bg-base-100 text-base-content">
-              <main>{props.children}</main>
-            </section>
+            <ClerkLoading>
+              <Nav logged={payload ? true : false} />
+              <div className="hero-content mx-auto max-w-sm">
+                <div className="hero-content mx-auto max-w-sm">
+                  <div className="flex justify-stretch items-stretch gap-4">
+                    <div className="flex flex-col gap-4 w-64">
+                      <div className="skeleton h-64 w-full"></div>
+                      <div className="skeleton h-32 w-full"></div>
+                      <div className="skeleton h-32 w-full"></div>
+                      <div className="skeleton h-32 w-full"></div>
+                    </div>
+                    <div className="flex flex-col gap-4 w-64">
+                      <div className="skeleton h-64 w-full"></div>
+                      <div className="skeleton h-32 w-full"></div>
+                      <div className="skeleton h-32 w-full"></div>
+                      <div className="skeleton h-32 w-full"></div>
+                    </div>                   
+                  </div>
+                </div>
+              </div>
+            </ClerkLoading>
+            <ClerkLoaded>
+              <Nav logged={payload ? true : false} />
+              <Toaster />
+              <section className="min-h-screen bg-base-100 text-base-content">
+                <main>{props.children}</main>
+              </section>
+            </ClerkLoaded>
             <Footer />
           </body>
         </html>
