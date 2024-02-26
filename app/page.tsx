3456b15d8ca471cf2d147fb3aeb38b3d9fbed69f
diegonicita <diegonicita@gmail.com>
2024-02-25 23:16:51 -0300

    add a seconda navbar to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { auth } from '@clerk/nextjs'
 import Home from './(pages)/home/page'
+import { Nav } from './components/navbar/navbar'
+import getInfoAuthCookie from './server-actions/helpers/getInfoAuthCookie'
 
-export default function IndexPage() {
-  return <Home />
+export default async function IndexPage() {
+  const { userId } = auth()
+  const payload = await getInfoAuthCookie(userId)
+  return (
+    <div>
+      <Nav logged={payload ? true : false} />
+      <Home />
+    </div>
+  )
 }
 
 export const metadata = {
