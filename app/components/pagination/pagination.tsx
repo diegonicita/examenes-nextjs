e00e46d6d313a8ef9c1cb97ee39bd45cf83f3af6
Ever Jose Rojas perez <everjosejr@hotmail.com>
2024-03-11 11:42:45 -0500

    add-hidebuttonpagination in page question

--- app/components/pagination/pagination.tsx
+++ app/components/pagination/pagination.tsx
@@ -3,47 +3,52 @@ import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 import React from "react";
 
-const Pagination = ({ totalQuestions}: { totalQuestions: number }) => {
-  const pathName = usePathname();
-  const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get("page")) || 1;
-  const createPageUrl = (page: string | number) => {
-    const params = new URLSearchParams(searchParams);
-    params.set("page", page.toString());
-    return `${pathName}?${params.toString()}`;
-  };
-  return (
-    <div className="join w-full mx-auto items-center justify-center gap-1 mb-4">
-      {currentPage === 1 ? (
-        <button disabled={currentPage === 1} className="btn btn-disabled">
-          Anterior
-        </button>
-      ) : (
-        <Link
-          href={createPageUrl(currentPage - 1)}
-          className="join-item btn btn-outline"
-        >
-          {" "}
-          Anterior
-        </Link>
-      )}
-      {currentPage >= totalQuestions? (
-        <button
-          disabled={currentPage >= totalQuestions}
-          className="btn btn-disabled"
-        >
-          Siguiente
-        </button>
-      ) : (
-        <Link
-          href={createPageUrl(currentPage + 1)}
-          className="join-item btn btn-outline"
-        >
-          Siguiente
-        </Link>
-      )}
-    </div>
-  );
+const Pagination = ({ totalQuestions }: { totalQuestions: number }) => {
+	const pathName = usePathname();
+	const searchParams = useSearchParams();
+	const currentPage = Number(searchParams.get("page")) || 1;
+	const createPageUrl = (page: string | number) => {
+		const params = new URLSearchParams(searchParams);
+		params.set("page", page.toString());
+		return `${pathName}?${params.toString()}`;
+	};
+	return (
+		<div className="join w-full mx-auto items-center justify-center gap-1 mb-4">
+			{currentPage === 1 ? (
+				<button
+					type="button"
+					disabled={currentPage === 1}
+					className="btn btn-disabled text-base"
+				>
+					Anterior
+				</button>
+			) : (
+				<Link
+					href={createPageUrl(currentPage - 1)}
+					className="join-item btn btn-outline"
+				>
+					{" "}
+					Anterior
+				</Link>
+			)}
+			{currentPage >= totalQuestions ? (
+				<button
+					type="button"
+					disabled={currentPage >= totalQuestions}
+					className="btn btn-disabled"
+				>
+					Siguiente
+				</button>
+			) : (
+				<Link
+					href={createPageUrl(currentPage + 1)}
+					className="join-item btn btn-outline"
+				>
+					Siguiente
+				</Link>
+			)}
+		</div>
+	);
 };
 
 export default Pagination;
