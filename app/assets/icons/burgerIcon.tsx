ee334ddb7eccc7e0001f7043d27c8f6baa5aec02
diegonicita <diegonicita@gmail.com>
2024-03-20 23:38:21 -0300

    fix title svg bugs and anchor bugs in footer

--- app/assets/icons/burgerIcon.tsx
+++ app/assets/icons/burgerIcon.tsx
@@ -1,25 +1,14 @@
 export function BurgerIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-    className="inline-block w-8 h-8 stroke-current"    
+      className="inline-block w-8 h-8 stroke-current"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M4 18L20 18"        
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
-      <path
-        d="M4 12L20 12"        
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
-      <path
-        d="M4 6L20 6"        
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
+      <title>Burger Icon</title>
+      <path d="M4 18L20 18" strokeWidth="2" strokeLinecap="round" />
+      <path d="M4 12L20 12" strokeWidth="2" strokeLinecap="round" />
+      <path d="M4 6L20 6" strokeWidth="2" strokeLinecap="round" />
     </svg>
   )
 }
