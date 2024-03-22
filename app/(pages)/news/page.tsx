f5243e8eb6ef48240a64fda8221a6dc7943d9dff
Ever Jose Rojas perez <everjosejr@hotmail.com>
2024-03-22 16:08:47 -0500

    Update page.tsx

--- app/(pages)/news/page.tsx
+++ app/(pages)/news/page.tsx
@@ -4,13 +4,14 @@ import type { UserType } from '@/app/models/User'
 import { redirect } from 'next/navigation'
 import News from '../home/components/news/news'
 import dataNews from './action/data/dataNews'
+import type { NewsItem } from '@/app/models/news/newData'
 
 export default async function newsPage() {
   const currentUser = (await getInfoAuthCookie()) as UserType
   if (currentUser?.role !== 'admin') {
     redirect('/')
   }
-  const data = await dataNews()
+  const data = (await dataNews()) as NewsItem[]
   return (
     <section className='flex flex-col items-center'>
       {' '}
