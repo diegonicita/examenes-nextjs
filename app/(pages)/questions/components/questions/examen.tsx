bcf8f70d1c2f23324bf6c22d065efd1dffaad9c9
Ever Jose Rojas perez <everjosejr@hotmail.com>
2024-03-27 11:20:12 -0500

    fix- style input comment in mobile

--- app/(pages)/questions/components/questions/examen.tsx
+++ app/(pages)/questions/components/questions/examen.tsx
@@ -1,41 +1,41 @@
-"use client";
-import type { QuestionSQL as QuestionType } from "@/app/models/QuestionSQL";
-import Question from "./question";
-import AllTheCommentContent from "../social/comments/allTheCommentContent/allthecommentContent";
-import type { SubjectType } from "@/app/models/Subject";
-import type { UserType } from "@/app/models/User";
+'use client'
+import type { QuestionSQL as QuestionType } from '@/app/models/QuestionSQL'
+import Question from './question'
+import AllTheCommentContent from '../social/comments/allTheCommentContent/allthecommentContent'
+import type { SubjectType } from '@/app/models/Subject'
+import type { UserType } from '@/app/models/User'
 
 export const Examen = ({
-	data,
-	userId,
-	temas,
-	currentUser,
-	treeComments,
+  data,
+  userId,
+  temas,
+  currentUser,
+  treeComments,
 }: {
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	data: any;
-	userId: number;
-	temas: SubjectType[];
-	currentUser: UserType;
-	treeComments: string[];
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  data: any
+  userId: number
+  temas: SubjectType[]
+  currentUser: UserType
+  treeComments: string[]
 }) => {
-	return (
-		<>
-			{data?.map((item: QuestionType) => (
-					<div
-						key={item.id}
-						className="w-full border border-gray-400 rounded my-4 px-4 pb-4"
-					>
-						<Question item={item} userId={userId} />
-						<AllTheCommentContent
-							item={item}
-							treeComments={treeComments}
-							currentUser={currentUser}
-						/>
-					</div>
-				))}
-		</>
-	);
-};
+  return (
+    <>
+      {data?.map((item: QuestionType) => (
+        <div
+          key={item.id}
+          className='w-full border border-gray-400 rounded my-4 px-4 pb-4'
+        >
+          <Question item={item} userId={userId} />
+          <AllTheCommentContent
+            item={item}
+            treeComments={treeComments}
+            currentUser={currentUser}
+          />
+        </div>
+      ))}
+    </>
+  )
+}
 
-export default Examen;
+export default Examen
